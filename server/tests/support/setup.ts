99153f3a142594e7799d343d78416679aa8270c0
Owen Greenaway <owengreenaway@hotmail.com>
2025-12-02 17:30:20 +0000

    Remove the old testing library

--- server/tests/support/setup.ts
+++ server/tests/support/setup.ts
@@ -1,25 +1,2 @@
-import supertest, { Test } from "supertest";
-
-import TestAgent from "supertest/lib/agent";
-
-/******************************************************************************
-                                    Run
-******************************************************************************/
-
-let agent: TestAgent<Test> | undefined;
-
-beforeAll(async () => {
-  try {
-    const app = (await import("@src/server")).default;
-    agent = supertest.agent(app);
-  } catch (error) {
-    // If app import fails, some tests may not work (for unit tests that don't need the server)
-    // This is expected for unit tests that don't require the Express app
-  }
-});
-
-/******************************************************************************
-                                    Export
-******************************************************************************/
-
-export { agent };
+// Jest setup file
+// No custom setup needed
